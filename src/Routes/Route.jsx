d7cdc772a417fc76a766259e2536337541a2ebae
gmnayem631 <gmnayem631@gmail.com>
2026-03-17 11:11:15 +0600

    added basic navigation bar, footer, and router

---
+++ src/Routes/Route.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../pages/Root/Root";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Home from "../pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    errorElement: ErrorPage,
+    children: [{ index: true, Component: Home }],
+  },
+]);
+
+export default router;
